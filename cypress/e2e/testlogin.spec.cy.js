27fd1b130a189728a37091d587c9ffc1f870c1dc
Adryano Junior <jusamsung10@gmail.com>
2024-09-10 10:14:30 -0300

    Adding user data json

--- cypress/e2e/testlogin.spec.cy.js
+++ cypress/e2e/testlogin.spec.cy.js
@@ -1,10 +1,13 @@
+import userData from '../fixtures/userData.json'
+
 describe('Login - Sucess and Login - Fail Step by Step', () => {
 
 const selectorsList = {
   usernameField: "[name='username']" ,
   passwordField: "[name='password']" ,
   loginButton: "[type='submit']" ,
-  selectionTitleDashboard: '.oxd-topbar-header-breadcrumb-module' , 
+  selectionTitleDashboard: '.oxd-topbar-header-breadcrumb-module' ,
+  dashboardUpgrade: '.orangehrm-upgrade-layout' , 
   wrongCredentialAlert: "[role='alert']"
 }
 
@@ -13,15 +16,15 @@ const selectorsList = {
   });
 
   it('Login - Sucess', () => {
-    cy.get(selectorsList.usernameField).type('Admin')
-    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.selectionTitleDashboard).contains('Dashboard')
+    cy.get(selectorsList.dashboardUpgrade)
   })
   it('Login - Fail', () => {
-    cy.get("[name='username']").type('Admin10')
-    cy.get("[name='password']").type('admin456')
+    cy.get("[name='username']").type(userData.userSuccess.username)
+    cy.get("[name='password']").type(userData.userFail.password)
     cy.get("[type='submit']").click()
     cy.get(selectorsList.wrongCredentialAlert)
   });
